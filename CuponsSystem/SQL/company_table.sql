2a1d91d6030918959f1afce7e9f81e24ba066b15
igor <igor@igor-Lenovo-B560>
2018-06-22 17:08:46 +0300

    Admin facade fixed company lable sql connection pool test

--- CuponsSystem/SQL/company_table.sql
+++ CuponsSystem/SQL/company_table.sql
@@ -1,25 +1,25 @@
 CREATE DATABASE CouponDB;
 USE CouponDB;
 CREATE TABLE `Company` (
-	`ID` int(15) NOT NULL UNIQUE,
-	`CompanyName` varchar(255),
-	`Password` varchar(255),
+	`ID` bigint(15) NOT NULL UNIQUE,
+	`CompanyName` varchar(64),
+	`Password` varchar(64),
 	`Email` varchar(255),
 	PRIMARY KEY (`ID`));
 CREATE TABLE `Customer` (
-	`ID` int(15) NOT NULL UNIQUE,
-	`CustomerName` varchar(255),
-	`Password` varchar(255),
+	`ID` bigint(15) NOT NULL UNIQUE,
+	`CustomerName` varchar(64),
+	`Password` varchar(64),
 	PRIMARY KEY (`ID`));
 CREATE TABLE `Coupon` (
-	`ID` int(15) NOT NULL UNIQUE,
-	`Title` varchar(255) ,
+	`ID` bigint(15) NOT NULL UNIQUE,
+	`Title` varchar(64) ,
 	`StartDate` DATE,
 	`EndDate` DATE,
 	`Amount`INTEGER,
-	`Type` varchar(255),
+	`Type` varchar(64),
 	`Message` varchar(255),
-	`Price` double precision,
+	`Price` double ,
 	`Image` varchar(255),
 	PRIMARY KEY (`ID`));
 CREATE TABLE `CustomerCoupon` (
